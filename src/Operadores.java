079d68f32dc2a2a14efbddb05b388d4624634ed0
aronresni <aronresni@gmail.com>
2024-02-27 11:22:33 -0300

    introduccionjava

---
+++ src/Operadores.java
@@ -0,0 +1,15 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int suma = 5 + 4;
+        int resta = 5-4;
+        int publicacion = 5*4;
+        int diviison = 10/2;
+        int resto = 10/3;
+
+        System.out.println("Suma ="+ suma);
+        System.out.println(
+        "Resto de la division ="+ resto
+        );
+    }
+
+}
---
+++ src/Variable.java
@@ -0,0 +1,19 @@
+public class Variable {
+    public static void main(String[] args) {
+
+        int entero;
+        double decimal;
+        String string;
+        boolean booleano;
+
+        entero = 10;
+        decimal = 3.2;
+        string = "Proyecto Java";
+        booleano = true;
+        System.out.println(string);
+
+
+
+
+    }
+}
