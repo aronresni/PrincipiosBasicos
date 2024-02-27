75b6640cb9eee36bcbcfd40ba28445d44b279bf6
aronresni <aronresni@gmail.com>
2024-02-27 11:22:48 -0300

    introduccionjava

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+int entero;
+double decimal;
+String string;
+boolean booleano;
+
+entero = 10;
+decimal = 3.2;
+string = "Proyecto Java";
+booleano = true;
+        System.out.println(string);
+}
+}
